21ce8ddb62d53b324241f55426854357f4fa48f4
baize-yanren <1062973115@qq.com>
2026-06-23 20:14:19 +0800

    修改数据集选择逻辑至文件内部

--- WP-RT-TRLAD/train.py
+++ WP-RT-TRLAD/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14cfc336b76e4f527ea269be4371b6ae64b338e851abb9c4009855157d220d6b
-size 20133
+oid sha256:1b11621c046ce29db6848d3227db56ae94ec4460a0a94dc60ba8010a0f70077c
+size 20376
