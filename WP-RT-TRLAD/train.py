bcc9853c0fd56514f66e351d7534b4bebd51da5b
baize-yanren <1062973115@qq.com>
2026-07-12 08:47:06 +0800

    chore: 更新训练代码和MSL数据集的评测指标结果
    更新了train.py的git-lfs文件指针以及msl数据集的通道评测指标结果文件

--- WP-RT-TRLAD/train.py
+++ WP-RT-TRLAD/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9000713cdf0271ba7aceba6993199e2a7bfbaa653c54166397064b4bafc5e17
-size 52410
+oid sha256:80245696a16709ff85d2f778418539eed0aae10cc8b6b45d831cb88e061606c4
+size 52989
