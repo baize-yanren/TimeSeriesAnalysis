f96dce491a1214e6fa18a87fa168b641a8ec1a91
baize-yanren <1062973115@qq.com>
2026-07-18 07:40:22 +0800

    chore: 完成WP-RT-TRLAD-v3基线版本训练与文档编写
    新增SWAT/PSM/MBA/MSL/SMAP五数据集训练结果，补充模型介绍文档，更新代码与缓存文件

---
+++ WP-RT-TRLAD-v3/model_introduction.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdfd59c348dd01aee8d2021850f2d29154a4f04c564c36e7f15fbaef16d3af1a
+size 13461
--- WP-RT-TRLAD-v3/preProcessing.py
+++ WP-RT-TRLAD-v3/preProcessing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f2ff311326cd45d5699a0ffec6782e38f9d6f5c812c04500f22051ff0157ee
-size 747
+oid sha256:e2d3c19d7bb8643d6b2847c80e7a2af40ac9b614c9a8d8bc80b0b6dcf1898059
+size 1171
